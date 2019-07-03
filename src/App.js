8c4291b11aa8f22b698a2ec90a7ffe41266a0e01
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-07-02 21:57:19 -0500

    Make call to API, configure .env file with key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,47 @@
 import React from 'react';
 import './App.css';
 
+fetch(`https://developer.nps.gov/api/v1/parks?stateCode=tx&api_key=${process.env.REACT_APP_API_KEY}`)
+  .then((res) => { return res.json() })
+  .then((data) => {
+
+    console.log(data)
+    let result = `<h2> National Parks in Texas </h2>`;
+
+    data.data.forEach((park) => {
+      const {
+        id,
+        description,
+        designation,
+        directionsInfo,
+        directionsUrl,
+        fullName,
+        latLong,
+        name,
+        parkCode,
+        states,
+        url,
+        weatherInfo
+      } = park
+      result +=
+        `<div>
+             <p>${id}</p>
+             <p>${description}</p>
+             <p>${designation}</p>
+             <p>${directionsInfo}</p>
+             <p>${directionsUrl}</p>
+             <p>${fullName}</p>
+             <p>${latLong}</p>
+             <p>${name}</p>
+             <p>${parkCode}</p>
+             <p>${states}</p>
+             <p>${url}</p>
+             <p>${weatherInfo}</p>
+              </div>`;
+    });
+    console.log(result)
+  })
+
 function App() {
   return (
     <div className="App">
