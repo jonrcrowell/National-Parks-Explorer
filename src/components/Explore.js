d4d9093701bd63f1056d560e3a6516f693faaa47
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-08-26 22:46:32 -0500

    Format file on save -- prettier got very aggressive in the last few days

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,111 +1,122 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import styled from 'styled-components'
+import styled from "styled-components";
 import "../App.css";
-import stateAbbreviations from '../stateAbbreviations'
-import Button from './Button'
-import Park from './Park'
+import stateAbbreviations from "../stateAbbreviations";
+import Button from "./Button";
+import Park from "./Park";
 import ParkTitle from "./ParkTitle";
 
 const Title = styled.p`
-    text-align: left;
-    font-size: .5em;
-    font-weight: 700;
+  text-align: left;
+  font-size: 0.5em;
+  font-weight: 700;
 `;
 
 const ParkLayout = styled.div`
-    display: grid; 
-    width: 80vw;
-    grid-template-columns: 1fr 2fr; 
-    grid-template-rows: 1fr; 
-    grid-column-gap: 20px;
-    grid-row-gap: 0px; 
+  display: grid;
+  width: 80vw;
+  grid-template-columns: 1fr 2fr;
+  grid-template-rows: 1fr;
+  grid-column-gap: 20px;
+  grid-row-gap: 0px;
 `;
 
 function Explore() {
-    const [parkState, updateParkState] = useState("tx");
-    const [parks, updateParks] = useState(null);
-    const [park, updatePark] = useState({
-        id: 1,
-        name: 'Pick a Park',
-        description: "Click on any of the parks in the list to the left for more details."
+  const [parkState, updateParkState] = useState("tx");
+  const [parks, updateParks] = useState(null);
+  const [park, updatePark] = useState({
+    id: 1,
+    name: "Pick a Park",
+    description:
+      "Click on any of the parks in the list to the left for more details."
+  });
+
+  useEffect(() => {
+    const parksUrl = `https://developer.nps.gov/api/v1/parks?stateCode=${parkState}&api_key=${process.env.REACT_APP_API_KEY}`;
+    axios.get(parksUrl).then(response => {
+      updateParks(response.data.data);
     });
+  }, [parkState]);
 
-    useEffect(() => {
-        const parksUrl = `https://developer.nps.gov/api/v1/parks?stateCode=${parkState}&api_key=${
-            process.env.REACT_APP_API_KEY
-            }`;
-        axios.get(parksUrl).then(response => {
-            updateParks(response.data.data);
-        });
-    }, [parkState]);
+  function handleParkStateClick(e, abbreviation) {
+    console.log(e);
+    updateParks(null);
+    updateParkState(abbreviation);
+    updatePark({
+      id: 1,
+      name: "Pick a Park",
+      description:
+        "Click on any of the parks in the list to the left for more details."
+    });
+  }
 
-    function handleParkStateClick(e, abbreviation) {
-        console.log(e)
-        updateParks(null);
-        updateParkState(abbreviation);
-        updatePark({
-            id: 1,
-            name: 'Pick a Park',
-            description: "Click on any of the parks in the list to the left for more details."
-        })
-    }
+  function handleParkClick(e, name) {
+    updatePark(parks.filter(x => x.name === name)[0]);
+  }
 
-    function handleParkClick(e, name) {
-        updatePark(parks.filter(x => x.name === name)[0]);
-    }
-
-    return (
-        <div>
-            <div className="App">
-                <header className="App-header">
-                    <div className="parent">
-                        {stateAbbreviations.map(ab => <Button active={false} key={ab} onClick={e => handleParkStateClick(e, ab)}>{ab}</Button>)}
-                    </div>
-                    {!parks ? <p>Searching for parks...</p> : <div>
-                        <Title>There are {parks.length} National Parks in {parkState.toUpperCase()}</Title>
-                        <ParkLayout className="parklayout">
-                            <div className="parkList">
-                                {parks.map(
-                                    ({
-                                        id,
-                                        name,
-                                        designation
-                                    }) => (
-                                            // TODO added on 8/22/2019 by Jon: highlight the selected park and have an arrow pointing to the detail
-                                            // TODO Need to put the list of parks in a scrollable section so that clicking
-                                            // TODO one will always display the park's details to the right so that the use
-                                            // TODO doesn't have to scroll around to see what they've picked
-                                            <ParkTitle
-                                                key={id}
-                                                name={name}
-                                                designation={designation}
-                                                onClick={e => handleParkClick(e, name)}
-                                            ></ParkTitle>
-                                        )
-                                )}
-                            </div>
-                            <div>
-                                <Park key={park.id}
-                                    description={park.description}
-                                    designation={park.designation}
-                                    directionsInfo={park.directionsInfo}
-                                    directionsUrl={park.directionsUrl}
-                                    fullName={park.fullName}
-                                    latLong={park.latLong}
-                                    name={park.name}
-                                    parkCode={park.parkCode}
-                                    states={park.states}
-                                    url={park.url}
-                                    weatherInfo={park.weatherInfo}
-                                ></Park>
-                            </div>
-                        </ParkLayout>
-                    </div>}
-                </header>
-            </div >
-        </div>
-    );
+  return (
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <div className="parent">
+            {/* Why is the default state (TX) not active when the page first loads? */}
+            {stateAbbreviations.map(ab => (
+              <Button
+                active={false}
+                key={ab}
+                onClick={e => handleParkStateClick(e, ab)}
+              >
+                {ab}
+              </Button>
+            ))}
+          </div>
+          {!parks ? (
+            <p>Searching for parks...</p>
+          ) : (
+            <div>
+              <Title>
+                There are {parks.length} National Parks in{" "}
+                {parkState.toUpperCase()}
+              </Title>
+              <ParkLayout className="parklayout">
+                <div className="parkList">
+                  {parks.map(({ id, name, designation }) => (
+                    // TODO added on 8/22/2019 by Jon: highlight the selected park and have an arrow pointing to the detail
+                    // TODO Need to put the list of parks in a scrollable section so that clicking
+                    // TODO one will always display the park's details to the right so that the use
+                    // TODO doesn't have to scroll around to see what they've picked
+                    <ParkTitle
+                      key={id}
+                      name={name}
+                      designation={designation}
+                      onClick={e => handleParkClick(e, name)}
+                    ></ParkTitle>
+                  ))}
+                </div>
+                <div>
+                  <Park
+                    key={park.id}
+                    description={park.description}
+                    designation={park.designation}
+                    directionsInfo={park.directionsInfo}
+                    directionsUrl={park.directionsUrl}
+                    fullName={park.fullName}
+                    latLong={park.latLong}
+                    name={park.name}
+                    parkCode={park.parkCode}
+                    states={park.states}
+                    url={park.url}
+                    weatherInfo={park.weatherInfo}
+                  ></Park>
+                </div>
+              </ParkLayout>
+            </div>
+          )}
+        </header>
+      </div>
+    </div>
+  );
 }
 
 export default Explore;
