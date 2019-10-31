212da9b27617f6d95667da2f2081a7b8c932b950
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-10-30 20:35:20 -0500

    Add logrocket to record user sessions
    Freaking amazing

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,47 +1,57 @@
-import React from 'react'
-import Ul from './Ul'
-import styled from "styled-components"
+import React from "react";
+import Ul from "./Ul";
+import styled from "styled-components";
 
 const Done = styled.li`
-    text-decoration: line-through;
-    color: #4A5568;
-`
+  text-decoration: line-through;
+  color: #4a5568;
+`;
 const Page = styled.div`
-    background-color: white;
-    padding: 3rem;
-`
+  background-color: white;
+  padding: 3rem;
+`;
 
 const Tasks = () => (
-    <Page>
-        <h2>Stuff I want to add to the site</h2>
-        <a
-            className="App-link"
-            href="https://www.nps.gov/subjects/developer/api-documentation.htm"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            National Park Service API Documentation
-            </a>
-        <Ul>
-            To Do List:
-            <li>Indicate active state button</li>
-            <li>Display the events for a park</li>
-            <li>Display the lesson plans for a park</li>
-            <li>Display visitor center information for a park</li>
-            <li>Display campground information for a park</li>
-            <li>Use a Styled Comoponents' Theme Provider to set global style variables like border radius and colors</li>
-            <li>Allow user to specify "favorite" parks</li>
-            <li>Create view for favorited parks</li>
-            <li>Consider storing data in my own db to speed up queries and update periodically with a service</li>
-            <Done>Remove padding from NavBar</Done>
-            <Done>Separate page content from nav bar</Done>
-            <Done>Use Styled Components to style the site</Done>
-            <Done>Display a list of parks for a given state</Done>
-            <Done>Create a styled "park" card and display search results with a grid of cards</Done>
-            <Done>Add routes</Done>
-            <Done>Add navigation</Done>
-        </Ul>
-    </Page>
+  <Page>
+    <h2>Stuff I want to add to the site</h2>
+    <a
+      className="App-link"
+      href="https://www.nps.gov/subjects/developer/api-documentation.htm"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      National Park Service API Documentation
+    </a>
+    <Ul>
+      To Do List:
+      <li>Indicate active state button for default</li>
+      <li>Display the events for a park</li>
+      <li>Display the lesson plans for a park</li>
+      <li>Display visitor center information for a park</li>
+      <li>Display campground information for a park</li>
+      <li>
+        Use a Styled Comoponents' Theme Provider to set global style variables
+        like border radius and colors
+      </li>
+      <li>Allow user to specify "favorite" parks</li>
+      <li>Create view for favorited parks</li>
+      <li>
+        Consider storing data in my own db to speed up queries and update
+        periodically with a service
+      </li>
+      <Done>Integrate Logrocket</Done>
+      <Done>Remove padding from NavBar</Done>
+      <Done>Separate page content from nav bar</Done>
+      <Done>Use Styled Components to style the site</Done>
+      <Done>Display a list of parks for a given state</Done>
+      <Done>
+        Create a styled "park" card and display search results with a grid of
+        cards
+      </Done>
+      <Done>Add routes</Done>
+      <Done>Add navigation</Done>
+    </Ul>
+  </Page>
 );
 
 export default Tasks;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import LogRocket from "logrocket";
+import setupLogRocketReact from "logrocket-react";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+LogRocket.init("qlmqem/dev-park-explorer");
+
+// after calling LogRocket.init()
+setupLogRocketReact(LogRocket);
+
+// This is an example script - don't forget to change it!
+LogRocket.identify("HOMEBOY", {
+  name: "Jonny Bolt",
+  email: "jbolt@mailinator.com",
+
+  // Add your own custom user variables here, ie:
+  subscriptionType: "supreme"
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
